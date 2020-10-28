e42baf7d8fcfb4869e69702011a994ab53bf8410
Douglas <douglaza@gmail.com>
2020-10-28 18:41:54 -0300

    comma fixed

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -3,4 +3,4 @@ db.produtos.createIndex(
   { descricao: "text" },
   { default_language: "portuguese" }
 );
-db.produtos.count({ $text: { $search: '"feito com"' } });
+db.produtos.count({ $text: { $search: "\"feito com\"" } });
