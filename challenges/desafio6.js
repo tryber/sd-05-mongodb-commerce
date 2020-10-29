d0dc6a652856eabe7c817d14236c1d052c16b2c6
Preto115 <preto115@gmail.com>
2020-10-29 20:18:33 -0300

    desafio_6.js

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -10,3 +10,10 @@ dos sanduíches Big Mac e Quarteirão com Queijo.
 Crie uma query que retorne o nome e ingredientes de todos os documentos.
 
  */
+
+db.produtos.updateMany(
+  { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+  { $push: { ingredientes: "bacon" } }
+);
+
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 }).pretty();
