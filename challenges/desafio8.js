4331d2cdb5c6cb3d4e7bcd4c5feadc768ea1dc74
ferrari <luisdiasferrari@gmail.com>
2020-10-29 18:17:37 -0300

    Desafio 08

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -3,3 +3,10 @@
 // Para isso, escreva no arquivo desafio8.js duas queries, nesta ordem:
 // Crie uma query que faça a remoção do primeiro ingrediente no sanduíche Quarteirão com Queijo.
 // Crie uma query que retorne o nome e ingredientes de todos os documentos.
+
+db.produtos.updateMany(
+  { "nome": { $in: [ "Quarteirão com Queijo" ] } },
+  { $pop: { "ingredientes": -1 } }
+);
+
+db.produtos.find({}, { "_id": 0, "nome": 1, "ingredientes": 1 }).pretty();
