d8b161381624da6c11b6d5114779bbde1ae7e6a9
luca <luucadecastro@gmail.com>
2020-10-29 01:15:48 -0300

    correcoes

--- challenges/Desafio2.js
+++
@@ -1,7 +0,0 @@
-db.produtos.updateMany(
-  {},
-  { valorUnitario: { $exists: false } },
-  { $set: { valorUnitario: NumberDecimal(0.0) } }
-);
-
-db.produtos.find({}, { nome: 1, valorUnitario: 1 });
