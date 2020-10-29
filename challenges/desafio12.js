6d5790ff198c9b911fc742c3b4e5155099d1942f
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-29 17:31:24 -0300

    challenge 12 fixed, push was missing before each

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,7 +1,7 @@
 // Crie uma query que faça em todos os documentos a ordenação dos elementos do array valoresNutricionais pelo campo percentual de forma descendente. Dica: mesmo sem adicionar nenhum novo elemento, para essa operação é necessário utilizar também o modificador $each.
 db.produtos.updateMany(
   {},
-  { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } }
+  { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } }
 );
 
 // Crie uma query que retorne o nome e valoresNutricionais de todos os documentos.
