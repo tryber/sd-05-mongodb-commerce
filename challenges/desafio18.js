455e5156ae3bf436fdd962714fbbdf720f2826be
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-29 17:32:39 -0300

    challenges 17 18 text only commented requirement

---
+++ challenges/desafio18.js
@@ -0,0 +1,3 @@
+// Crie uma query que faça a criação de um índice do tipo text no campo descricao com o idioma padrão portuguese.
+
+// Crie uma query que retorne a quantidade de documentos que contêm a expressão feito com utilizando o operador $text.
