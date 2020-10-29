331ec421c22b29fa91d8320d7fe5489e5b70675b
ferrari <luisdiasferrari@gmail.com>
2020-10-29 18:50:34 -0300

    Desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -7,3 +7,20 @@
 // Crie uma query que incremente as vendas de Big Mac às quartas-feiras em 60.
 // Crie uma query que incremente as vendas de todos os sanduíches de carne do tipo bovino e pão aos sábados em 120.
 // Crie uma query que retorne o nome e vendasPorDia de todos os documentos.
+
+db.produtos.updateMany(
+  {},
+  { $set: { "vendasPorDia": [0, 0, 0, 0, 0, 0, 0] } }
+);
+
+db.produtos.updateMany(
+  { "nome": "Big Mac" },
+  { $set: { "vendasPorDia.3": 60 } }
+);
+
+db.produtos.updateMany(
+  { $and: [ {"tags": "bovino"}, {"tags": "pão"} ] },
+  { $set: { "vendasPorDia.6": 120 } }
+);
+
+db.produtos.find({}, {"_id": 0, "nome": 1, "vendasPorDia": 1}).pretty();
