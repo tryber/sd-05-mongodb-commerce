7857f8057d4a6384cccef74ecc8c9fdc2e9f62d1
Christian Duessel <duessel@gmail.com>
2020-10-29 16:15:27 -0300

    req3

---
+++ challenges/desafio3.js
@@ -0,0 +1,25 @@
+db.produtos.updateMany(
+    {},
+    {
+        $set: {"avaliacao": NumberInt(0)}
+    }
+);
+
+db.produtos.updateMany(
+    {tags: {$not: {$all: ["ave"]}}},
+    {
+        $inc: {avaliacao: 5}
+    }
+);
+
+db.produtos.updateMany(
+    {tags: {$all: ["ave"]}},
+    {
+        $inc: {avaliacao: 3}
+    }
+);
+
+db.produtos.find(
+    {},
+    { _id: 0, nome: 1, avaliacao: 1}
+);
