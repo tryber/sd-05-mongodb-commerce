681a3dd10318a62874d6e423a62bd76e5ddf23c6
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-29 17:25:44 -0300

    trying to find error in 10 13 14

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -4,7 +4,7 @@
 db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
 
 //Crie uma query que incremente as vendas de Big Mac às quartas-feiras em 60.
-db.produtos.updateOne({ nome: "Bic Mac" }, { $inc: { "vendasPorDia.3": 60 } });
+db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 
 //Crie uma query que incremente as vendas de todos os sanduíches de carne do tipo bovino e pão aos sábados em 120.
 db.produtos.updateMany(
--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -5,7 +5,7 @@ db.produtos.updateMany(
       $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } },
     },
   },
-  { push: { tags: "contém sódio" } }
+  { $push: { tags: "contém sódio" } }
 );
 
 // Crie uma query que retorne o nome e tags de todos os documentos.
