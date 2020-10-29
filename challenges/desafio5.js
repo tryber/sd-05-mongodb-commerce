beeed87b7f8fc3de4e1280c6524823ac7c434ef1
Mau <mauricio.sdjr@gmail.com>
2020-10-29 18:56:42 -0300

    corrigindo desafio 5 e 6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
   {"nome": {$ne: "McChicken"}},
-  {$addToSet: {"igredientes": "ketchup"}}
+  {$addToSet: {"ingredientes": "ketchup"}}
 );
 
 db.produtos.find({}, { "nome" : 1, "ingredientes": 1, "_id": 0 });
