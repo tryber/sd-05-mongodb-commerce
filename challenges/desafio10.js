347a122fc90b6184a3e553fb6a5dbcbab5e35345
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-29 16:43:56 -0300

    challenge 10 fixed updateMany mistake

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -7,7 +7,7 @@ db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
 db.produtos.updateOne({ name: "Bic Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 
 //Crie uma query que incremente as vendas de todos os sanduíches de carne do tipo bovino e pão aos sábados em 120.
-db.produtos.updateOne(
+db.produtos.updateMany(
   { tags: { $in: ["pão", "bovino"] } },
   { $inc: { "vendasPorDia.6": 120 } }
 );
