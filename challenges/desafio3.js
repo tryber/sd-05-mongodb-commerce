ffefcab97d214c9b046f21efd88ab542831fdec9
Mau <mauricio.sdjr@gmail.com>
2020-10-29 17:04:07 -0300

    desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,18 @@
+db.produtos.updateMany(
+  {},
+  {$set: { "avaliacao": NumberInt("0") }}
+);
+
+db.produtos.updateMany(
+  {"tags": "bovino"},
+  {$inc: { "avaliacao": 5 }}
+);
+
+db.produtos.updateMany(
+  {"tags": "ave"},
+  {$inc: { "avaliacao": 3 }}
+);
+
+db.produtos.find({}, { "nome" : 1, "avaliacao": 1, "_id": 0 });
+
+db.produtos.find({nome: {$in:["Quarteirão com Queijo", "Cheddar McMelt"]}});
