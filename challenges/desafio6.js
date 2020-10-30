b96b44651a4064e8e1dc8c4ef36ac805fb168f08
hleoc <informig@gmail.com>
2020-10-30 20:44:39 -0300

    Desafio 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,2 @@
+db.produtos.updateMany({$and: {"nome": "Big Mac", "nome": "Quarteirão com Queijo"}}, {$addToSet: {"ingredientes": "bacon"}});
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1});
