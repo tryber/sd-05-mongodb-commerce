e60e7e056d76cf5cf0e62c21d0376dddaef423d7
Preto115 <preto115@gmail.com>
2020-10-30 15:56:14 -0300

    desafio_10.js

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -18,10 +18,16 @@ Crie uma query que retorne o nome e vendasPorDia de todos os documentos.
 
  */
 
-db.produtos.updateMany({}, { $push: { "vendasPorDia": { $each: [0, 0, 0, 0, 0, 0, 0]}}});
+db.produtos.updateMany(
+  {},
+  { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } }
+);
 
-db.produtos.updateOne({nome: "Big Mac"}, {$inc: {"vendasPorDia.3": 60}});
+db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 
-db.produtos.updateMany({tags: {$in: ["bovino","pão"] }}, {$inc: {"vendasPorDia.6": 120}});
+db.produtos.updateMany(
+  { tags: { $in: ["bovino", "pão"] } },
+  { $inc: { "vendasPorDia.6": 120 } }
+);
 
-db.produtos.find({}, {nome: 1, vendasPorDia: 1, "_id": 0});
+db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
