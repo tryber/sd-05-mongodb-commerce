5d706f1534b5ab92acabc700486736f7376b5093
luiz-avelar <luiz.avelar25@gmail.com>
2020-10-31 18:28:33 -0300

    Challenges 1 to 12

---
+++ challenges/desafio2.js
@@ -0,0 +1,19 @@
+// Inclua o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.
+
+// Para isso, escreva no arquivo desafio2.js duas queries, nesta ordem:
+
+// Crie uma query que adicione o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.
+
+// Crie uma query que retorne o nome e valorUnitario de todos os produtos.
+
+// Referência: https://stackoverflow.com/questions/24824657/how-do-i-update-mongodb-document-fields-only-if-they-dont-exist
+
+db.produtos.updateMany(
+  { valorUnitario: { $exists: false } },
+  { $set: { valorUnitario: NumberDecimal("0.00") } }
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, valorUnitario: 1 }
+);
