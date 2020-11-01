23c9e526d5eaf605d2e0cd38aab2036a83df5b0f
hleoc <informig@gmail.com>
2020-10-31 21:39:40 -0300

    Desafios 10 e 11 - corrigindo os desafios parte 1

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,2 +1,2 @@
-db.produtos.updateOne({}, {$addToSet: {tags: {$each: ["combo", "tasty"]}}});
+db.produtos.updateOne({}, {$addToSet: {tags: {$each: ["combo", "tasty"], $sort: {tags: 1}}}});
 db.produtos.find({}, {_id: 0, nome: 1, tags: 1});
