a50205c96add686a255cf9f43df36fa86b7ab5ac
RenataNovais <rnovaiscs@gmail.com>
2020-11-01 14:57:48 -0300

    forçando reavaliação desafio 18

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -6,8 +6,8 @@
 // Crie uma query que faça a criação de um índice do tipo text no campo descricao com o idioma padrão portuguese.
 
 // Crie uma query que retorne a quantidade de documentos que contêm a expressão feito com utilizando o operador $text.
+// https://docs.mongodb.com/manual/tutorial/specify-language-for-text-index/ - HA
 
-// https://docs.mongodb.com/manual/tutorial/specify-language-for-text-index/
 db.produtos.createIndex({descricao: "text"}, {default_language: "portuguese"});
 
 db.produtos.count({$text: {$search: "\"feito com\""}});
