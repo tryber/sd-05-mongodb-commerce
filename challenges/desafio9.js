c0fe20b3ee8dc86dea73cf754a2634e2b80dfab7
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-11-01 16:44:42 -0300

    Desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,10 @@
+// Remova o último ingrediente do sanduíche Cheddar McMelt.
+// Para isso, escreva no arquivo desafio9.js duas queries, nesta ordem:
+// Crie uma query que faça a remoção do último ingrediente no sanduíche Cheddar McMelt.
+// Crie uma query que retorne o nome e ingredientes de todos os documentos
+
+// Para remover o último ingrediente uso do operador $pop com o valor 1
+
+db.produtos.updateOne({ nome: "Cheddar McMelt"},{ $pop: { ingredientes: 1 }});
+
+db.produtos.find({}, { nome:1, ingredientes:1, _id:0 });
