a3fafaa7429c2ed3d06bf5486defa6253058d54c
hleoc <informig@gmail.com>
2020-10-31 23:06:41 -0300

    Desafios 13 e 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,2 @@
+db.produtos.updateMany({$and: [{"valoresNutricionais.tipo": "sódio", "valoresNutricionais.percentual": {$gt: 20}, "valoresNutricionais.percentual": {$lt: 40}}]}, {$push: {tags: ["muito sódio"]}});
+db.produtos.find({}, {_id: 0, "nome": 1, "tags": 1});
