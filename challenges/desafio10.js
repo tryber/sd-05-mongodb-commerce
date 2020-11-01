318fd0395a087dc88fcb9c7f92d25752dc0c9c69
hleoc <informig@gmail.com>
2020-10-31 21:52:21 -0300

    Desafios 10 e 11 - corrigindo os desafios parte 2

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,4 @@
-db.produtos.updateOne({}, {$addToSet: {"vendasPorDia": {$each: [0, 0, 0, 0, 0, 0, 0]}}});
-db.produtos.updateOne({"nome": "Big Mac"}, {$set: {"vendasPorDia.3": 60}});
-db.produtos.updateMany({$and: [{tags: [ "bovino", "pão" ]}]}, {$set: {"vendasPorDia.6": 120}});
-db.produtos.find({}, {_id: 0, nome: 1, "vendasPorDia": 1});
+db.produtos.updateMany({}, {$addToSet: {"vendasPorDia": {$each: [0, 0, 0, 0, 0, 0, 0]}}});
+db.produtos.updateMany({"nome": "Big Mac"}, {$set: {"vendasPorDia.3": 60}});
+db.produtos.updateMany({$and: [{"tags": [ "bovino", "pão" ]}]}, {$set: {"vendasPorDia.6": 120}});
+db.produtos.find({}, {_id: 0, "nome": 1, "vendasPorDia": 1});
--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,1 +1,2 @@
+/* db.produtos.updateMany({"valoresNutricionais"}, {}); */
 /* db.produtos.find({}, {_id: 0, nome: 1, "valoresNutricionais": 1}); */
