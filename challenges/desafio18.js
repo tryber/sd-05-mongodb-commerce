1df87764bd74f2825e8efab9fe3cc2dae521e4d2
RenataNovais <rnovaiscs@gmail.com>
2020-11-01 15:06:05 -0300

    alterando expressao no desafio 18

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -10,4 +10,4 @@
 
 db.produtos.createIndex({descricao: "text"}, {default_language: "portuguese"});
 
-db.produtos.count({$text: {$search: "\"feito com\""}});
+db.produtos.count({$text: {$search: "feito com"}});
