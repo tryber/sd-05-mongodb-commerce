633321bf33f2aaf7fe84d086c352b1d11ede7e76
hleoc <informig@gmail.com>
2020-11-02 11:46:27 -0300

    Desafio 14 - corrigindo os desafio parte 4

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,2 +1,9 @@
-/* db.produtos.updateMany({}, {}); */
-/* db.produtos.find({}, {_id: 0, nome: 1, "valoresNutricionais": 1}); */
+/* db.produtos.updateMany({"valoresNutricionais.percentual": {$exists: true}}, {"valoresNutricionais": $each: ["percentual": 0], $sort: {percentual: -1}});
+db.produtos.find({}, {_id: 0, nome: 1, "valoresNutricionais": 1});
+
+$each: [
+valoresNutricionais
+
+percentual
+
+db.produtos.find({"ultimaModificacao": {$exists: true}}, {_id: 0, nome: 1}); */
