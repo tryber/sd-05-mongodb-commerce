fe2ee7b21b03d6658af1b36b82bcbf5eae2540f3
hleoc <informig@gmail.com>
2020-11-02 13:59:21 -0300

    Desafio 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,9 +1,2 @@
-/* db.produtos.updateMany({"valoresNutricionais.percentual": {$exists: true}}, {"valoresNutricionais": $each: ["percentual": 0], $sort: {percentual: -1}});
+db.produtos.updateMany({}, {$push: {"valoresNutricionais": {$each: [], $sort: {"percentual": -1}}}});
 db.produtos.find({}, {_id: 0, nome: 1, "valoresNutricionais": 1});
-
-$each: [
-valoresNutricionais
-
-percentual
-
-db.produtos.find({"ultimaModificacao": {$exists: true}}, {_id: 0, nome: 1}); */
