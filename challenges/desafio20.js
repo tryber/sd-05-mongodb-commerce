37c12a5304f3038f7378a40f2bebdcc450fa63e4
ferrari <luisdiasferrari@gmail.com>
2020-11-02 14:29:29 -0300

    desafio 20

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -3,3 +3,9 @@
 // Para isso, escreva no arquivo desafio20.js duas queries, nesta ordem:
 // Crie uma query que faça a remoção do campo curtidas do item Big Mac.
 // Crie uma query que retorne o nome e curtidas de todos os documentos.
+
+db.produtos.updateMany(
+  {"nome": "Big Mac"}, { $unset:{"curtidas": "" }}
+);
+
+db.produtos.find({}, {"_id": 0, "nome": 1, "curtidas": 1}).pretty();
