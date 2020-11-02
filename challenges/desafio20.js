ac36b3cea6c626d0a6316eda210e7cea8a0ea740
hleoc <informig@gmail.com>
2020-11-02 18:17:15 -0300

    Desafio 20

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany({nome: "Big Mac"}, {$pull: {"curtidas": / /i}});
+db.produtos.updateMany({nome: "Big Mac"}, {$unset: {curtidas: ""}});
 db.produtos.find({}, {_id: 0, nome: 1, curtidas: 1});
