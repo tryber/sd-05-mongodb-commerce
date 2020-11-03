7497d10bd3dab2dd73ece5f428cbdf3f4268a459
marinabrcls <barcelos.marina@gmail.com>
2020-11-03 13:39:33 -0300

    prettier

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,23 +2,30 @@
 // Para isso, escreva no arquivo desafio1.js duas queries, nesta ordem:
 
 // Crie uma query que adicione o campo criadoPor em todos os documentos, colocando "Ronald McDonald" no valor desse campo.
-db.produtos.updateMany({}, {
+db.produtos.updateMany(
+  {},
+  {
     $set: {
-        "criadoPor": "Ronald McDonald"
-    }
-});
+      criadoPor: "Ronald McDonald",
+    },
+  }
+);
 
 // - consulta prévia ao banco para ver a estrutura do documento: db.produtos.find() ou findOne();
 // - o método updateMany() permite que vários documentos que satisfaçam o critério de filtro sejam alterados de uma única vez;
+// - updateMany() recebe dois parâmetros: query (filtro) e projection (o que será alterado)
 // - nesse caso não há necessidade de nenhum filtro porque queremos alterar todos os documentos;
 // - operador $set altera o valor de um campo específico e, se o campo não existir, o $set adiciona um novo campo com o valor especificado;
 
 // Crie uma query que retorne o nome e criadoPor de todos os produtos.
-db.produtos.find({}, {
-    "_id": 0, 
-    nome: 1, 
-    "criadoPor": 1
-});
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    criadoPor: 1,
+  }
+);
 
 // - o método find() seleciona documentos de uma coleção e retorna um cursor com esses documentos;
 // - o find() recebe como parâmetros uma query (filtro de seleção) e uma projection (especifica quais campos serão retornados);
