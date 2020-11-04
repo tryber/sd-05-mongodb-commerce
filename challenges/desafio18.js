59d4125432b42ffdbe0e8865c30e0094a2937a2d
RenataNovais <rnovaiscs@gmail.com>
2020-11-04 16:46:28 -0300

    teste numero 1000000

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,16 +1,3 @@
-// Desafio 18
-// Conte quantos documentos contêm a expressão 'feito com' utilizando o operador $text.
-
-// Para isso, escreva no arquivo desafio18.js duas queries, nesta ordem:
-
-// Crie uma query que faça a criação de um índice do tipo text no campo descricao com o idioma padrão portuguese.
-
-// Crie uma query que retorne a quantidade de documentos que contêm a expressão feito com utilizando o operador $text.
-// https://docs.mongodb.com/manual/tutorial/specify-language-for-text-index/ - HA
 db.produtos.createIndex({"descricao": "text"}, {default_language: "portuguese"});
 
-db.produtos.find({
-  $text: {
-    $search: "\"feito com\""
-  }
-}).count();
+db.produtos.count({$text: {$search: "\"feito com\""}});
