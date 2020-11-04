fa2dd18b029c82a0070a48286ea83a2f50189d05
Mau <mauricio.sdjr@gmail.com>
2020-11-04 18:23:44 -0300

    corrigindo desafios 20 e 21

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,6 +1,6 @@
-db.produtos.updateOne(
+db.produtos.updateMany(
   {"nome": "Big Mac"},
   {$unset: {"curtidas": ""}}
 );
 
-db.produtos.find({}, { "nome" : 1, "_id": 0 });
+db.produtos.find({}, { "nome" : 1, "curtidas": 1, "_id": 0 });
