e7a0b5bd4e47f1267386ae8d79d075b262bf8780
Marylange <marylangess@gmail.com>
2020-11-09 18:13:52 -0300

    verificando requisito 19

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -2,4 +2,6 @@
 Desafio 15
 Conte quantos produtos contêm Mc no nome, sem considerar letras maiúsculas ou minúsculas.
 */
-db.produtos.find({ $text: { $search: "\"Mc\"", $caseSensitive: false } }).count();
+db.produtos.find(
+  { $text: { $search: "\"Mc\"", $caseSensitive: false } }
+).count();
